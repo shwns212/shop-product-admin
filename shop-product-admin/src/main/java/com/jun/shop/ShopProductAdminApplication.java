7039786a9609435093b59b662a342fc9df8b654c
shwns212 <shwns212@gmail.com>
2021-01-27 00:28:04 +0900

    first commit

---
+++ shop-product-admin/src/main/java/com/jun/shop/ShopProductAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jun.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProductAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProductAdminApplication.class, args);
+	}
+
+}
